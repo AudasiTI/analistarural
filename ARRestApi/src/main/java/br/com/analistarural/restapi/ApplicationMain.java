8da7308876b41220380c4831b0eef0bd8d3164d6
jocemar <jocemarfg@audasi.com.br>
2016-01-26 17:19:58 -0200

    * Inclusão das dependências de css e js dentro do projeto

--- ARRestApi/src/main/java/br/com/analistarural/restapi/ApplicationMain.java
+++ ARRestApi/src/main/java/br/com/analistarural/restapi/ApplicationMain.java
@@ -1,10 +1,17 @@
 package br.com.analistarural.restapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ApplicationMain {
 
